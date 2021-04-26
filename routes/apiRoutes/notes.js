908757819f34096c85bdbdc41977f28e2c062833
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-04-26 08:36:18 -0700

    added ids

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -14,7 +14,7 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
-  //  req.body.id = notesDb.length.toString();
+   req.body.id = notesDb.length.toString();
   if (!validateNote(req.body)) {
     res.status(400).send('The note is invalid.');
   } else {
